6c40ff1ee5870796758143b3b9fcd901bdcdaec7
beatriztorresdossant <157639966+beatriztorresdossant@users.noreply.github.com>
2024-02-01 17:35:40 -0300

    primeiro usuario criado

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,27 @@ import { db, firestore } from '../banco-de-dados/firebase'
 
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('bem vinda ao meu app')
 })
 
 app.post('/usuario', async (req, res) => {
-    const usuario = req.body.nome
+    const nome  = req.body.nome;
+    const email = req.body.email
+    const telefone = req.body.telefone
 
     try {
         const docRef = await firestore.addDoc(firestore.collection(db, 'usuario'), {
             nome: nome,
+            email: email,
+            telefone: telefone
         })
 
-        res.send(docRef.id)
+        res.send("usuario adicionado com sucesso:" + docRef.id)
     } catch (e) {
-
+        console.log(e)
     }
 })
 
