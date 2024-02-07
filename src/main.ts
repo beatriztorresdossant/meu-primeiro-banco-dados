5bebf3c8013b087edfd2ad2dc1ef3b27d66c3e79
beatriztorresdossant <157639966+beatriztorresdossant@users.noreply.github.com>
2024-02-07 17:12:09 -0300

    deletei um usuario

--- src/main.ts
+++ src/main.ts
@@ -23,9 +23,60 @@ app.post('/usuario', async (req, res) => {
 
         res.send("usuario adicionado com sucesso:" + docRef.id)
     } catch (e) {
-        console.log(e)
+        console.log("Erro ao adicionar usuario: ", e)
+
+        res.status(500).send(e);
     }
-})
+});
+
+app.get('/listarUsuarios', async (req, res) => {
+    try {
+      const usuarios = await firestore.getDocs(firestore.collection(db, 'usuario'))
+  
+      const usuariosLista = usuarios.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+      res.send(usuariosLista)
+    } catch (e) {
+      console.log("Erro ao listar usuários: " + e)
+      
+      res.status(500).send("Erro ao listar usuários: " + e)
+    }
+  })
+  
+  app.put('/atualizarUsuario/:id', async (req, res) =>{
+    const id = req.params.id
+    const nome = req.body.nome
+  
+    try {
+      await firestore.updateDoc(firestore.doc(db, 'usuario', id), {
+        nome: nome,
+      })
+      res.send('usuario atualizado com sucesso!!')
+    } catch (e) {
+      console.log('Erro ao atualizar usuário: ' + e)
+  
+      res.status(500).send('Erro ao atualizar o usuário: ' + e )
+    }
+  
+  })
+
+  app.delete('/deletarUsuario/:id' , async (req , res) =>{
+    const id =req.params.id
+    try {
+        await firestore.deleteDoc(firestore.doc(db, 'usuarios', id))
+        res.send ('Usuario deletado com sucesso')
+        
+
+
+    } catch (e) {
+        console.log('Erro ao deletar usuario:' + e)
+
+        res.status(500).send('Erro ao deletar: ')
+
+    }
+  })
 
 app.listen(3000, function () {
     console.log('serviço rodando em http://localhost:3000')
